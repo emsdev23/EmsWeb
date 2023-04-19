0e4da7a04b48f6d5426633b8503d328def430f48
emsdev23 <129821095+emsdev23@users.noreply.github.com>
2023-04-19 20:55:17 +0530

    Add files via upload

--- datagraphs/src/pages/Alerts.js
+++ datagraphs/src/pages/Alerts.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import Table from 'react-bootstrap/Table';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 function Alerts() {
   return (
     <div>
-      <div> 
-        
+      <div style={{display: "flex",
+  alignitems: "flex-right"}}> 
+      <Dropdown >
+      <Dropdown.Toggle variant="warning" id="dropdown-basic" style={{marginTop:"30px",width:"300px"}} >
+        <b> Active Alerts </b>
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        <Dropdown.Item href="/Peakdemand">Peak Demand</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Chiller</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Themal</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
       </div>
      
     <Table striped bordered hover variant="dark"  style={{marginTop:"50px"}}>
