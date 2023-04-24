8b4c8639fcb619ac441bc5cca4a24e6869e19233
emsdev23 <129821095+emsdev23@users.noreply.github.com>
2023-04-24 17:17:05 +0530

    Add files via upload

---
+++ datagraphs/src/pages/ChillerAlerts.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import Table from 'react-bootstrap/Table';
+
+function ChillerAlerts() {
+  return (
+    <div>
+      <div >
+        <h1 style={{textAlign:'center',marginTop:"30px"}}><b>Chiller</b></h1>
+      </div>
+
+<Table striped bordered hover variant="dark"  style={{marginTop:"50px"}}>
+      <thead>
+        <tr>
+          <th>Alert Name</th>
+          <th>Parameter</th>
+          <th>Limit set</th>
+          <th>Action taken</th>
+          <th>Severity</th>
+        
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Common header outlet temperature breach</td>
+          <td>Common header outlet temperature </td>
+          <td>Common header outlet temperature limit has crossed 10 deg-C</td>
+          <td>Alert Email</td>
+          <td>High</td>
+          
+        </tr>
+        {/* <tr>
+          <td>13/4/2023</td>
+          <td>Chiller</td>
+          <td>Common Header Outlet Temparature limit has crossed 10°C</td>
+          <td>10:46:40 am</td>
+          <td>High</td>
+          <td>Mail sent</td>
+        </tr> */}
+      </tbody>
+    </Table>
+    </div>
+  )
+}
+
+export default ChillerAlerts
