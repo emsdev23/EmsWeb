6db8520eae86c17f383fc5fb0f0b3696d0b3ef47
emsdev23 <129821095+emsdev23@users.noreply.github.com>
2023-07-07 13:57:17 +0530

    Add files via upload

--- datagraphs/src/App.js
+++ datagraphs/src/App.js
@@ -1,21 +1,10 @@
-
 import './App.css';
-// import Sidebar from './main';
-// import Data from './Api'
-
 import * as React from 'react';
 import Navbar from './components/Navbar';
-
-// import Navebar from './Navebar'
-// import Analytics from './analytics';
-// import Alerts from './alerts';
-// import Control from './control';
-// import Documentation from './documentation'
-// import Dashboard from './Dashboard'
-import DashBoard from './pages/DashBoard'
-import Analytics from './pages/Analytics'
+import DashBoard from './pages/DashBoard';
+import Analytics from './pages/Analytics';
 import Control from './pages/Control';
-import Alerts from './pages/Alerts'
+import Alerts from './pages/Alerts';
 import Documentation from './pages/Documentation';
 import Peakdemand from './pages/Peakdemand';
 import Thermalalers from './pages/Thermalalers';
@@ -23,62 +12,56 @@ import ChillerAlerts from './pages/ChillerAlerts';
 import RooftopSolar from './pages/RooftopSolar';
 import Wheeledinsolar from './pages/Wheeledinsolar';
 import axios from 'axios';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import { ToastContainer, toast } from 'react-toastify';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import swal from 'sweetalert';
 import Thermalcontrol from './pages/Thermalcontrol';
 import Peakdemandgraph from './pages/Peakdemandgraph';
 
-
-
-function App() {
-
+function LocationContext(props) {
+  const location = useLocation();
+  const isDocumentationRoute = location.pathname === '/Battery_Analytics';
 
   return (
-    <> 
-    <div > 
-
-    
-    <BrowserRouter>
-    <Navbar/> 
-    <Routes> 
-      <Route  path ='/' element={<DashBoard/>}/>
-      <Route  path ='/Analytics' element={<Analytics/>}/>
-      <Route  path ='/Control/upsbattery' element={<Control/>}/>
-      <Route  path ='/Control/thermal' element={<Thermalcontrol/>}/>
-      
-      <Route  path ='/Alertlogs' element={<Alerts/>}/>
-      <Route  path ='/Documentation' element={<Documentation/>}/>
-      <Route  path ='/Alerts/Peakdemand' element={<Peakdemand/>}/>
-      <Route  path ='/Alerts/Thermalalers' element={<Thermalalers/>}/>
-      <Route  path ='/Alerts/ChillerAlerts' element={<ChillerAlerts/>}/>
-      <Route  path ='/peakgraph' element={<Peakdemandgraph/>}/>
-      <Route  path ='/Wheeledgraph' element={<Wheeledinsolar/>}/>
-      <Route  path ='/RoofTopSolar' element={<RooftopSolar/>}/>
-      
-
-      </Routes>
-
-    
-    </BrowserRouter>
-
-    {/* <ToastContainer /> */}
-    </div>
-
-
-    
-   
- 
-
-
-
+    <>
+      {/* Conditionally render the Navbar component */}
+      {!isDocumentationRoute && <Navbar />}
+      {props.children}
     </>
   );
 }
 
+function App() {
+  return (
+    <BrowserRouter>
+      <LocationContext>
+        <div>
+          <Routes>
+            <Route path="/" element={<DashBoard />} />
+            <Route path="/Analytics" element={<Analytics />} />
+            <Route path="/Control/upsbattery" element={<Control />} />
+            <Route path="/Control/thermal" element={<Thermalcontrol />} />
+            <Route path="/Alertlogs" element={<Alerts />} />
+            <Route path="/Battery_Analytics" element={<Documentation />} />
+            <Route path="/Alerts/Peakdemand" element={<Peakdemand />} />
+            <Route path="/Alerts/Thermalalers" element={<Thermalalers />} />
+            <Route path="/Alerts/ChillerAlerts" element={<ChillerAlerts />} />
+            <Route path="/peakgraph" element={<Peakdemandgraph />} />
+            <Route path="/Wheeledgraph" element={<Wheeledinsolar />} />
+            <Route path="/RoofTopSolar" element={<RooftopSolar />} />
+          </Routes>
+        </div>
+      </LocationContext>
+    </BrowserRouter>
+  );
+}
+
 export default App;
+
+
+
 
 
 
