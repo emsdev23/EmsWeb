b4e68f2d5ff58b14c516cfab119137920eadd3c2
emsdev23 <129821095+emsdev23@users.noreply.github.com>
2023-07-07 14:11:16 +0530

    Add files via upload

--- datagraphs/src/pages/Alerts.js
+++ datagraphs/src/pages/Alerts.js
@@ -3,11 +3,20 @@ import Table from 'react-bootstrap/Table';
 import Dropdown from 'react-bootstrap/Dropdown';
 import axios from 'axios';
 import { useState,useEffect } from 'react';
+import swal from 'sweetalert';
 
 function Alerts() {
   
 const host = "121.242.232.211"
 const [alerts,setAlerts]=useState([])
+const [filterAlert,setFilterAlert]=useState("")
+const [receivedData,setReceivedDate]=useState([])
+
+
+     //onchange function for alert filter
+     const handleAlertChange = (event) => {
+      setFilterAlert(event.target.value);
+    };
 
   const AlertsData=()=>{
     axios.get(`http://${host}:5000/Alert/Logs`).then((res)=>{
@@ -19,10 +28,57 @@ const [alerts,setAlerts]=useState([])
     })
   } 
 
+  const FilterAlertsData = async () => {
+
+    axios.post('http://localhost:5000/Alerts/filter', { systemName: filterAlert })
+    .then(response => {
+      const Dataresponse=response.data
+          //console.log(response.data);
+      setReceivedDate(Dataresponse)
+    })
+    .catch(error => {
+      // Handle the error
+      console.error(error);
+    });
+  };
+  
+
+//   if(Dataresponse.lengt>0){
+//     //console.log(response.data);
+// setReceivedDate(Dataresponse)
+// }
+// else if(Dataresponse.lengt<0){
+//   swal({
+//     title: "Alert",
+//     text: 'no matched alert found ',
+//     icon: "warning",
+//     buttons: true,
+//     dangerMode: true,
+//     background: 'darkblue'
+//   })
+// }
+  
+
   useEffect(()=>{
     AlertsData()
   },[])
+
+  useEffect(()=>{
+    FilterAlertsData()
+  },[filterAlert])
+ 
+
+
+  console.log(filterAlert)
+  console.log(receivedData)
+
+
+  // useEffect(()=>{
+  //   AlertsData()
+  // },[])
+
 console.log(alerts)
+
 
 
 
@@ -31,42 +87,64 @@ console.log(alerts)
     <div >
         <h1 style={{textAlign:'center',marginTop:"30px"}}><b>Alert Logs</b></h1>
       </div>
-      {/* <Dropdown >
-      <Dropdown.Toggle variant="warning" id="dropdown-basic" style={{marginTop:"30px",width:"300px"}} >
-        <b> Active Alerts </b>
-      </Dropdown.Toggle>
-
-      <Dropdown.Menu>
-        <Dropdown.Item href="/Peakdemand">Peak Demand</Dropdown.Item>
-        <Dropdown.Item href="/ChillerAlerts">Chiller</Dropdown.Item>
-        <Dropdown.Item href="/Thermalalers">Thermal</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown> */}
   
+  <select
+        className="form-select"
+        aria-label="Default select example"
+        style={{ width: "20%",marginLeft:"100px"}}
+        value={filterAlert}
+        onChange={handleAlertChange}
+      
+      >
+  <option selected>Filter Alerts</option>
+  <option value="Building Load">Building consumption</option>
+  <option value="Chiller">Chillers</option>
+  <option value="Thermal Storage">Thermal</option>
+</select>
+<br/>
+{/* value={selectedMonth}
+ onChange={handleMonthChange} */}
      
-      <Table striped bordered hover variant="dark" style={{ marginTop: "50px" }}>
+      <Table striped bordered hover variant="dark" style={{ marginTop: "50px"}}>
   <thead>
     <tr>
       <th>Date</th>
       <th>System Name</th>
       <th>Alert</th>
+      <th>LimitValue</th>
       <th>Time</th>
       <th>Severity</th>
       <th>Action</th>
     </tr>
   </thead>
   <tbody>
-    {alerts.map((data) => (
+  {filterAlert ? (
+    receivedData.map((val) => (
+      <tr key={val.id}>
+        <td>{val.alerttimereceived[0]}</td>
+        <td>{val.systemName}</td>
+        <td>{val.alert}</td>
+        <td>{val.limitvalue}(kWh)</td>
+        <td>{val.alerttimereceived[1]}</td>
+        <td>{val.severity}</td>
+        <td>{val.action}</td>
+      </tr>
+    ))
+  ) : (
+    alerts.map((data) => (
       <tr key={data.id}>
         <td>{data.alerttimereceived[0]}</td>
         <td>{data.systemName}</td>
         <td>{data.alert}</td>
+        <td>{data.limitvalue}(kWh)</td>
         <td>{data.alerttimereceived[1]}</td>
         <td>{data.severity}</td>
         <td>{data.action}</td>
       </tr>
-    ))}
-  </tbody>
+    ))
+  )}
+</tbody>
+
 </Table>
 
 
