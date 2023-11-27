44c30702ae22ab3b79d823218291f86847067b00
emsdev23 <129821095+emsdev23@users.noreply.github.com>
2023-11-27 15:55:15 +0530

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,6 @@ const cors = require("cors");
 const nodemailer = require('nodemailer');
 const moment = require('moment-timezone');
 const tz = 'Asia/Kolkata'
-const password = "Arun@1807"
 const email = 'arun.kumar@tenet.res.in'
 const emailto =['ems@respark.iitm.ac.in']
 
